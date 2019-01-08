682cb480f1193276a92f0ce4f20cc2375a3c1863
Till Hainbach <till.hainbach@uni-potsdam.de>
2019-01-08 22:21:48 +0100

    fixed -folder default argument handling

--- handle.py
+++ handle.py
@@ -8,7 +8,7 @@ import sys
 _LOG_LEVELS_STR = ['INFO', 'WARNING', 'ERROR', 'DEBUG']
 
 default_conf = { 'spotitagger':
-                 { 'folder'                 : os.path.join(os.path.expanduser('~'),'Music'),
+                 { 'folder'                 : None,
                    'overwrite-metadata'     : True,
                    'log-level'              : 'INFO',
                    'username'               :  None }
@@ -24,7 +24,7 @@ def log_leveller(log_level_str):
 def merge(default, config):
     """ Override default dict with config dict. """
     merged = default.copy()
-    merged.update({ k: v for k,v in config.items() if v })
+    merged.update({ k: v for k,v in config.items() if v and not v=='prompt'})
     return merged
 
 def get_config(config_file):
@@ -74,15 +74,10 @@ def get_arguments(raw_args = None, to_group = True, to_merge = True):
         group.add_argument(
             '-p', '--playlist', help='tag songs based on preexisting Spotify playlist')
 
-    #default_folder = config['folder']
-    #if default_folder:
+
     parser.add_argument(
-        '-f', '--folder', default=os.path.relpath(config['folder'], os.getcwd()),
-            help = 'path to folder where songs are stored in', required = True)
-    # else:
-    #     parser.add_argument(
-    #         '-f', '--folder', help = 'path to folder where songs are stored in',
-    #         required = True)
+        '-f', '--folder', default=config['folder'],
+            help = 'path to folder where songs are stored in')
     parser.add_argument(
         '-ll', '--log-level', default=config['log-level'],
         choices = _LOG_LEVELS_STR,
@@ -104,6 +99,8 @@ def get_arguments(raw_args = None, to_group = True, to_merge = True):
     if parsed.config is not None and to_merge:
         parsed = override_config(parsed.config,parser)
 
+    if not parsed.folder:
+        parser.error('Please use -f/--folder option to provide a folder or specifiy in config.yml')
     parsed.log_level = log_leveller(parsed.log_level)
 
     return parsed
