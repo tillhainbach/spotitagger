b5355c262600ef272330fa01c7fc904f17057169
Till Hainbach <till.hainbach@uni-potsdam.de>
2019-01-16 15:35:24 +0100

    added threading for embedding

--- spotitagger.py
+++ spotitagger.py
@@ -8,9 +8,11 @@ import const
 import pprint
 import platform
 import sys
-import multiprocessing
+import threading
+import unicodedata
 
-def list_all_m4a_files(path):
+
+def yield_all_m4a_files(path):
     '''return a generator object for all .m4a-files in the directory "path".
     Only audiofile in M4A-format are support by this script at the moment.'''
     for file in os.listdir(path):
@@ -23,24 +25,36 @@ def get_music_file_metadata(song):
                     "songname %-% songartist.m4a"
     so the song title can be clearly distinguished from the artist name.
     Spotify also uses " - " in song titles eg. when it is a remix.'''
-    filetitle, fileartist = song.split(" %-% ")
+    # macos uses different encoding for special characters then spotify.
+    filetitle, fileartist = (unicodedata.normalize('NFC', s) for s in song.split(" %-% "))
     fileartist = fileartist[:-4]
     return (filetitle, fileartist)
 
 def find_music_files_in_playlist(playlist_tracklist, audiofiles):
     '''for every audiofile return its index (e.g. position) in the spotify playlist.
-    returns a list of indexes'''
-    idx = []
+    returns a dictionary of indexes'''
+    idx = {}
+    log.debug('length of audiofiles is: %i' % len(audiofiles))
     for file in audiofiles:
         filetitle, fileartist = get_music_file_metadata(file)
+        idx[file] = None
         for i, dict in enumerate(playlist_tracklist):
+            if filetitle == 'The Melody (feat. Francesco Tristano, Les Siècles & François-Xavier Roth)':
+                s = dict['track']['name']
+                if s.startswith('The Melody'):
+                    print('file title is %s' % filetitle)
+                    print('playlist title ist %s' % s)
+                    if filetitle == s:
+                        print('the Melody found in playlist')
             if filetitle == dict['track']['name']:
                 artists = []
                 for artist in dict['track']['artists']:
                     artists.append(artist['name'])
                 artists = ', '.join(artists)
                 if set (fileartist) == set(artists):
-                    idx.append(i)
+                    idx[file] = i
+        if not idx[file]:
+            log.warning("{} not found in spotify playlist".format(file))
     return idx
 
 def nice_print(Object):
@@ -54,55 +68,42 @@ def benchmark (func, *args, **kwargs):
     elapsed_time = timeit.default_timer() - start_time
     print ('%s took %.5f seconds' % (func.__name__,elapsed_time))
 
-def embed_multi(track, path, overwrite):
-    music_file = track [0]
-    trackinfo = track [1]
-    metadata.embed(path, music_file, trackinfo, overwrite = overwrite)
-
 def main_playlist(uri, path, audiofiles):
     '''main function, looks up all audio files in path in playlist uri and embeds
     the metadata taken from spotify web api into the respective audio file.'''
     sp = SpotifyHelperTools(const.args.username)
-    result = sp.sp.playlist(uri, fields = 'tracks.items')['tracks']['items']
+    result = sp.fetch_playlist_tracks(uri)
     if not result:
         # abort if an empty spotify playlist was provided
         log.warning('empty Spotify playlist. Exiting program')
         return
-    music_files = {audiofile : index for audiofile, index in zip(audiofiles, find_music_files_in_playlist(result, audiofiles)) if index}
-    if not music_files:
+    audiofiles_in_playlist = {key : value for key, value in find_music_files_in_playlist(result, audiofiles).items() if value is not None}
+
+    log.info("{} out of {} tracks found in spotify playlist".format(len(audiofiles_in_playlist), len(audiofiles)))
+    if not audiofiles_in_playlist:
         # abort if none of the audiofiles are in the spotify playlist
         log.warning('None of the audiofiles are in the spotify playlist.')
         return
-    tracks = {file : SpotifyTrackObject(result[i]['track']) for file, i in music_files.items()}
-    print(len(tracks.keys()))
+    tracks = {file : SpotifyTrackObject(result[i]['track']) for file, i in audiofiles_in_playlist.items()}
     sp.complete_tracks_tags(list(tracks.values()))
 
-    # multiprocessing part
-    # slower for single track tagging
-    # To Do: implementation of multi-core argument needed
-    ListOfFilenames = list(tracks.items())
-    ListOfProcesses = []
-    Processors = 4
-    Parts = [ListOfFilenames[i:i + Processors] for i in range(0, len(ListOfFilenames), Processors)]
+    threads = []
+    count = 0
+    all_threads = [threading.Thread(target=metadata.embed, args=(path, music_file, track, const.args.overwrite)) for music_file, track in tracks.items()]
 
-    for part in Parts:
-        for track in part:
-            p = multiprocessing.Process(target=embed_multi, args=(track, path, const.args.overwrite))
-            # p = multiprocessing.Process(target = print, args = track)
-            p.start()
-            ListOfProcesses.append(p)
-        for p in ListOfProcesses:
-            p.join()
+    for t in all_threads:
+        count +=1
+        print('embedding %i tracks...' % count, end = ' \r')
+        t.start()
 
-    # single process method:
-    # i = 1
-    # for music_file, track in tracks.items():
-    #     print('embedding tracks... %i tracks embedded.' % i, end = ' \r')
-    #     metadata.embed(path, music_file, track, overwrite = const.args.overwrite)
-    #     i += 1
-
-
-
+    print('')
+    for i, t in enumerate(all_threads):
+        procent = i/len(all_threads) * 100
+        t.join()
+        print('%.1f done...' % procent, end = ' \r')
+    
+    print('100% Done!   ')
+    print('%i tracks embedded.' % count)
 
 if __name__ == '__main__':
     # get arguments/default arguments
@@ -117,7 +118,7 @@ if __name__ == '__main__':
     try:
         # get all audiofiles in directory first and check whether there are any.
         # abort if none.
-        audiofiles = list(list_all_m4a_files(const.args.folder))
+        audiofiles = list(yield_all_m4a_files(const.args.folder))
         if not audiofiles:
             log.warning('No .m4a files in directory. This program only supports audiofiles in M4A-Format. Exiting program')
             sys.exit(0)
